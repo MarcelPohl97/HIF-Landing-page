6e325013b4d35ac3cfa8b1f03ee9356ec3098576
Marcel <Marcel.Pohl1234@gmx.de>
2020-03-08 20:27:51 +0100

    added nav item hover animation, created microcomponent css folder

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@ const nav_btn = document.getElementById("nav-btn");
 const responsive_nav = document.getElementById("responsive-nav");
 const nav_options = document.getElementById("nav-options");
 
+
 activate_nav = () => {
+    nav_btn.classList.toggle("nav-active-state");
+    nav_btn.classList.toggle("nav-hover-state")
     responsive_nav.classList.toggle("nav-activator")
     nav_options.classList.toggle("nav-ul-activator")
 }
 
 nav_btn.addEventListener("click", () => {
     activate_nav();
-})
+});
